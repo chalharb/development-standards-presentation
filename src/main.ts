1cca57f15d630e1c88d6852b76323bb40f34ddb3
Logan Harber <chalharb@gmail.com>
2023-05-18 21:25:39 -0500

    feat: Integrated Reveal.js into project (#3)
    * feat: Integrated project with reveal.js
    
    * build: Updated tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
-import "./style.css";
+import Reveal from "reveal.js";
 
-console.log("hello world");
+// Plugins
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+
+// Styles
+import "reveal.js/dist/reset.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/black.css";
+import "reveal.js/plugin/highlight/monokai.css";
+import "./style.scss";
+
+let deck = new Reveal({
+  progress: true,
+  plugins: [Markdown, RevealHighlight],
+});
+
+deck.initialize();
