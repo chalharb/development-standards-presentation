65d20409407c0876ff8118f0e33cd04d9ac935d4
Logan Harber <chalharb@gmail.com>
2023-05-18 20:24:59 -0500

    fix: Resolved incorrect base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  base: "https://chalharb.github.io/development-standards-presentation/",
+  base: "/development-standards-presentation/",
 });
